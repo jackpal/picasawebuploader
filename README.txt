656a267f30b45f0fb96262c70cc361a84a69bae8
Jack Palevich <jack.palevich@gmail.com>
2011-09-11 14:20:27 -0700

    Added utilites for finding duplicate directories.

---
+++ README.txt
@@ -0,0 +1,16 @@
+A script that uploads photos to Picasa Web Albums
+
++ Resizes large images to be less than the free limit (2048 x 2048)
++ Takes a list of directories
++ Creates the albums as protected
+
+
+Installation
+
++ Prerequesites:
+  + Python 2.7
+  + Google Data APIs http://code.google.com/apis/gdata/
+    + gdata-2.0.14 for Python
+  + ImageMagick http://www.imagemagick.org/
+     + sudo port install ImageMagick
+
--- main.py
+++ main.py
@@ -1,14 +1,15 @@
 import argparse
+import filecmp
 import gdata.photos.service
 import gdata.media
 import gdata.geo
+import os
 
 def parseArgs():
   parser = argparse.ArgumentParser(description='Upload pictures to picasa web albums / Google+.')
   parser.add_argument('--email', help='the google account email to use (example@gmail.com)', required=True)
   parser.add_argument('--password', help='the password', required=True)
-  parser.add_argument('--source', help='the photo to upload', required=True)
-  parser.add_argument('--album', help='the album to upload to', required=True)
+  parser.add_argument('--source', help='the directory to upload', required=True)
 
   args = parser.parse_args()
   return args
@@ -39,19 +40,87 @@ def findOrCreateAlbum(gd_client, title):
   album = findAlbum(gd_client, title)
   if not album:
     print "Creating album " + title
+    # public, private, protected. private == "anyone with link"
     album = gd_client.InsertAlbum(title=title, summary='test album', access='private')
   return album
 
-def postPhoto(gd_client, email, album, filename):
-  album_url = '/data/feed/api/user/%s/albumid/%s' % (email, album.gphoto_id.text)
+def postPhoto(gd_client, album, filename):
+  album_url = '/data/feed/api/user/%s/albumid/%s' % (gd_client.email, album.gphoto_id.text)
   photo = gd_client.InsertPhotoSimple(album_url, 'New Photo', 
       'Uploaded using the API', filename, content_type='image/jpeg')
   return photo
 
+def postPhotoToAlbum(gd_client, photo, album):
+  album = findOrCreateAlbum(gd_client, args.album)
+  photo = postPhoto(gd_client, album, args.source)
+  return photo
+
+allExtensions = {}
+
+# key: extension, value: type
+knownExtensions = {
+  '.png': 'image/png',
+  '.jpeg': 'image/jpeg',
+  '.jpg': 'image/jpeg',
+  '.avi': 'video/avi',
+  '.wmv': 'video/wmv',
+  '.3gp': 'video/3gp',
+  '.m4v': 'video/m4v',
+  '.mp4': 'video/mp4',
+  '.mov': 'video/mov'
+  }
+
+def getMimeType(filename):
+  ext = os.path.splitext(filename)[1].lower()
+  if ext in knownExtensions:
+    return knownExtensions[ext]
+  else:
+    return None
+
+def accumulateSeenExtensions(filename):
+  ext = os.path.splitext(filename)[1].lower()
+  if ext in allExtensions:
+    allExtensions[ext] = allExtensions[ext] + 1
+  else:
+    allExtensions[ext] = 1
+
+def isMediaFilename(filename):
+  accumulateSeenExtensions(filename)
+  return getMimeType(filename) != None
+
+def visit(arg, dirname, names):
+  basedirname = os.path.basename(dirname)
+  if basedirname.startswith('.'):
+    return
+  mediaFiles = [name for name in names if not name.startswith('.') and isMediaFilename(name) and
+    os.path.isfile(os.path.join(dirname, name))]
+  count = len(mediaFiles)
+  if count > 0:
+    arg[dirname] = {'files': mediaFiles}
+
+def findMedia(source):
+  hash = {}
+  os.path.walk(source, visit, hash)
+  return hash
+
+def findDupDirs(photos):
+  d = {}
+  for i in photos:
+    base = os.path.basename(i)
+    if base in d:
+      print "duplicate " + base + ":\n" + i + ":\n" + d[base]
+      dc = filecmp.dircmp(i, d[base])
+      print dc.diff_files
+    d[base] = i
+  # print [len(photos[i]['files']) for i in photos]
+  
 def main():
   args = parseArgs()
-  gd_client = login(args.email, args.password)
+  # gd_client = login(args.email, args.password)
   # listAlbums(gd_client)
-  album = findOrCreateAlbum(gd_client, args.album)
-  postPhoto(gd_client, args.email, album, args.source)
+  # postPhotoToAlbum(gd_client, album, args.source)
+  photos = findMedia(args.source)
+  # findDupDirs(photos)
+  print photos
+  
 main()
